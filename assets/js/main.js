0a6bab89b475d997f35c7d2f4043199756d3dce0
mohdaltafdbd <112413513+mohdaltafdbd@users.noreply.github.com>
2025-08-15 01:33:50 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,17 @@ if (btn && nav) {
 // year
 const y = document.getElementById('year');
 if (y) y.textContent = new Date().getFullYear();
+
+document.querySelectorAll('.carousel').forEach(carousel => {
+  let images = carousel.querySelectorAll('img');
+  let index = 0;
+
+  function showNextImage() {
+    images[index].classList.remove('active');
+    index = (index + 1) % images.length;
+    images[index].classList.add('active');
+  }
+
+  images[0].classList.add('active');
+  setInterval(showNextImage, 3000); // change every 3 sec
+});
